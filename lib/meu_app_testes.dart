cc7ba980d4ec9d8735108a465f7490475617c474
Lavynia Barbosa <lavyniabs19@gmail.com>
2023-09-05 14:03:22 -0300

    Testes

---
+++ lib/meu_app_testes.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+double calcularDesconto(double valor, double desconto, bool percentual) {
+  if (valor <= 0) {
+    throw ArgumentError("O valor do produto não pode ser zero");
+  }
+  if (desconto <= 0) {
+    throw ArgumentError("O valor do descontoo não pode ser zero");
+  }
+  if (percentual) {
+    return valor - ((valor * desconto / 100));
+  }
+  return valor - desconto;
+}
+
+String convertToUpper(String texto) {
+  return texto.toUpperCase();
+}
+
+double retornaValor(double valor) {
+  return valor;
+}
+
+
